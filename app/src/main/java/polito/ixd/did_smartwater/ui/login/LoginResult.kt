c5d97679bf2b394a7899d90a583a7d26ce437dcb
LucaDonalisio <donalisioluca11@gmail.com>
2022-03-30 16:34:14 +0200

    navigation graph implemented

---
+++ app/src/main/java/polito/ixd/did_smartwater/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package polito.ixd.did_smartwater.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
---
+++ app/src/main/java/polito/ixd/did_smartwater/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package polito.ixd.did_smartwater.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import polito.ixd.did_smartwater.data.LoginDataSource
+import polito.ixd.did_smartwater.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
